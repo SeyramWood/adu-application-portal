3c43db2fc15c85d714767e6913fc2c6e09d98da7
SeyramWood <prikahaeyram@gmail.com>
2022-01-11 00:57:15 +0000

    Added subscribers

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'subscribe-to-newsletter'
+        'dashboard/subscribe-to-newsletter'
     ];
 }
